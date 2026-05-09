3574d32948bc276053b9fac9d0412d01ce9aafec
Jade-rie <riesenjade@gmail.com>
2026-05-09 11:32:27 +0200

    delete bin build

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mac/TDprog/C++/S2/osmGraph2/build/_deps/raylib-src
-# Build directory: /Users/mac/TDprog/C++/S2/osmGraph2/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
